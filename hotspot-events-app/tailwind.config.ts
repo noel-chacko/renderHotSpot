d8f4f3b6cbf4978352d623a69f7d8d5fc5920020
ewongzz <toernestw@yahoo.com>
2024-11-21 23:38:10 -0500

    trending events icon and animation

--- hotspot-events-app/tailwind.config.ts
+++ hotspot-events-app/tailwind.config.ts
@@ -8,10 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundSize: {
-        'gradient-size': '200% 200%',
-      },
-      animation:{
+      animation: {
+        flame: 'flame 1.5s infinite',
         'slide-in': 'slideIn 1.3s ease-out forwards',
         'gradient-flow': 'gradient-flow 5s ease-in-out infinite',
       },
@@ -38,16 +36,8 @@ const config: Config = {
           '0%': { transform: 'translateX(-100%)', opacity: '1' },
           '100%': { transform: 'translateX(0)', opacity: '2' },
         },
-
       },
-        'gradient-flow': {
-          '0%': { backgroundPosition: '0% 0%' },
-          '50%': { backgroundPosition: '100% 100%' },
-          '100%': { backgroundPosition: '0% 0%' },
-        },
-
-      }
-    
+    },
   },
   plugins: [],
 };
