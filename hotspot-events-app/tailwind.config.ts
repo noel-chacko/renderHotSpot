c60e64e4ff3d07584b9e78409405556b60912c83
Henry Nguyen <tuo91182@temple.edu>
2024-11-22 10:43:16 -0500

    event page from searchBar with moving background

--- hotspot-events-app/tailwind.config.ts
+++ hotspot-events-app/tailwind.config.ts
@@ -8,15 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-
-      animation: {
-        flame: 'flame 1.5s infinite',
-        'slide-in': 'slideIn 1.3s ease-out forwards',
-        'gradient-flow': 'gradient-flow 5s ease-in-out infinite',
-      },
-
       backgroundSize: {
         'gradient-size': '200% 200%',
+      },
+      animation:{
+        'slide-in': 'slideIn 1.3s ease-out forwards',
+        'gradient-flow': 'gradient-flow 5s ease-in-out infinite',
       },
       colors: {
         background: "var(--background)",
@@ -48,6 +45,7 @@ const config: Config = {
           '50%': { backgroundPosition: '100% 100%' },
           '100%': { backgroundPosition: '0% 0%' },
         },
+
       }
     
   },
