a540a78cea7eecc00e13347ed9ab374891590991
Henry Nguyen <tuo91182@temple.edu>
2024-11-25 12:45:32 -0500

    Search bar and Event details will now go to the same page. Added save events to calendar and enhanced UI

--- hotspot-events-app/src/app/page.tsx
+++ hotspot-events-app/src/app/page.tsx
@@ -13,7 +13,12 @@ export default function Home() {
 
       <main className="flex-grow flex items-center justify-center">
         <div className="flex flex-col space-y-4 mb-12 mr-auto">
-          <h1 className="welcome-text text-6xl font-bold ml-5 text-[#3D52A0]">Welcome to HOTSPOT</h1>
+        <h1 className="relative text-6xl font-bold text-white ml-5 animate-flipIn">
+          <span className="absolute inset-0 -translate-x-1 translate-y-1 text-[#2A3C73] blur-sm">
+            Welcome to HOTSPOT
+          </span>
+          <span className="relative">Welcome to HOTSPOT</span>
+        </h1>
           <Search />
         </div>
         <EventCatalog />
